d8147ddea2acd56fc352d029de56e74eb282be57
waldnershih <waldner0043@gmail.com>
2022-06-25 15:51:24 +1000

    lets exercise project init

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Home, NotFound } from "./pages";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from "react-router-dom";
+import history from "./history";
+
+import "./App.scss";
+
+const App = () => {
+	return (
+		<Router history={history}>
+			<Routes>
+				<Route path="/" exact element={<Home />} />
+				<Route path="/404" exact element={<NotFound />} />
+				<Route path="*" element={<Navigate replace to="/404" />} />
+			</Routes>
+		</Router>
+	);
+};
+
+export default App;
